275573ac047dbb1c342bb2f6dec48716e6cafc8d
Hsin-Ju Lin <astroneuro@Hsin-Jus-MacBook-Pro.local>
2017-10-17 00:16:30 +0800

    first commit-three functional button

---
+++ extension-danceParty/Helpers.swift
@@ -0,0 +1,9 @@
+//
+//  Helpers.swift
+//  extension-danceParty
+//
+//  Created by Hsin-Ju Lin on 17/10/2017.
+//  Copyright © 2017 spaceneurocruzz. All rights reserved.
+//
+
+import Foundation
---
+++ extension-danceParty/UIButtonExt.swift
@@ -0,0 +1,9 @@
+//
+//  UIButtonExt.swift
+//  extension-danceParty
+//
+//  Created by Hsin-Ju Lin on 16/10/2017.
+//  Copyright © 2017 spaceneurocruzz. All rights reserved.
+//
+
+import Foundation
